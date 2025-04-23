af2e79232fe6c57bd1d3e26c12c8b499abf7867a
Marc Lückert <marc.lueckert@buhlergroup.com>
2025-04-23 11:09:40 +0200

    refactor: Simplify token handling in Probe function

--- pkg/probe/probe.go
+++ pkg/probe/probe.go
@@ -63,12 +63,8 @@ func (p *ProbeCollector) Probe(ctx context.Context, target map[string]string, hc
 		Host:   tgt.Host,
 	}
 
-	if target["token"] != "" && savedConfig.AuthKeys[config.Target(target["target"])].Token == "" {
-		// Add the target and its apikey to the savedConfig and use, if exists, a target entry as a template for include/exclude
-		// This will only happend the "first" time
-		savedConfig.AuthKeys[config.Target(target["target"])] = config.TargetAuth{Token: config.Token(target["token"]),
-			Probes: savedConfig.AuthKeys[config.Target(target["profile"])].Probes}
-	}
+	savedConfig.AuthKeys[config.Target(target["target"])] = config.TargetAuth{Token: config.Token(target["token"]),
+		Probes: savedConfig.AuthKeys[config.Target(target["profile"])].Probes}
 
 	c, err := fortiHTTP.NewFortiClient(ctx, u, hc, savedConfig)
 	if err != nil {
